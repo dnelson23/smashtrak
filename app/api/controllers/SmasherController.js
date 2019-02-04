6223273b1c3a856aca4378a435d3aef113be22d5
Drew Nelson <drwnlsn@gmail.com>
2019-02-03 22:53:19 -0800

    Converts SmasherController.find to async function and users async methods

--- app/api/controllers/SmasherController.js
+++ app/api/controllers/SmasherController.js
@@ -1,5 +1,4 @@
-/**
- * SmasherController.js
+/* SmasherController.js
  * Drew Nelson
  * Oct 13 2016
  *
@@ -8,73 +7,34 @@
 
  module.exports = {
 
- 	find: function(req, res) {
- 		Smasher
- 		.findOne({ community: req.params.commID, tag: req.params.sTag })
- 		.populate('wins')
- 		.populate('losses')
- 		.populate('placings')
- 		.populate('community')
- 		.then(function(smasher) {
- 			if(smasher) {
- 				smasher = smasher.toObject();
- 				// complete all associated models and replace them
- 				async.map(smasher.wins, function(win, mapCb) {
- 					Match
- 					.findOne(win.id)
- 					.populate('winner')
- 					.populate('loser')
- 					.populate('tournament')
- 					.exec(function(err, win) {
- 						if(err) return mapCb(err);
- 						return mapCb(null, win);
- 					});
- 				}, function(err, wins) {
- 					if(err) return res.negotiate(err);
+ 	find: async function(req, res) {
+		try {
+			var smasher = await Smasher.findOne({ community: req.params.commID, tag: req.params.sTag })
+			.populate('wins')
+			.populate('losses')
+			.populate('placings')
+			.populate('community');
 
- 					smasher.wins = wins ? wins : [];
- 					// populate losses
- 					async.map(smasher.losses, function(loss, mapCb) {
- 						Match
- 						.findOne(loss.id)
- 						.populate('winner')
- 						.populate('loser')
- 						.populate('tournament')
- 						.exec(function(err, loss) {
- 							if(err) return mapCb(err);
- 							return mapCb(null, loss);
- 						});
- 					}, function(err, losses) {
- 						if(err) return res.negotiate(err);
+			// wins array
+			for(var i = 0; i < smasher.wins.length; i ++) {
+				smasher.wins[i] = await Match.findOne({ id: smasher.wins[i].id }).populate('winner').populate('loser').populate('tournament');
+			}
+			
+			// losses array
+			for(var i = 0; i < smasher.losses.length; i ++) {
+				smasher.losses[i] = await Match.findOne({ id: smasher.losses[i].id }).populate('winner').populate('loser').populate('tournament');
+			}
 
- 						smasher.losses = losses ? losses : [];
- 						// populate placings
- 						async.map(smasher.placings, function(placing, mapCb) {
- 							Placing
- 							.findOne(placing.id)
- 							.populate('smasher')
- 							.populate('tournament')
- 							.exec(function(err, place) {
- 								if(err) return mapCb(err);
- 								return mapCb(null, place);
- 							});
- 						}, function(err, placings) {
- 							if(err) return res.negotiate(err);
+			// placings array
+			for(var i = 0; i < smasher.placings.length; i++) {
 
- 							// Sort tournaments by smashers placing
- 							placings.sort(function(x, y) { return x.place - y.place; });
- 							smasher.placings = placings ? placings : [];
- 							return res.view('smasher/index', { smasher: smasher, community: smasher.community });
- 						});
- 					});
- 				});
- 			} else {
- 				return res.notFound('Could not find smasher');
- 			}
- 		})
- 		.catch(function(err) {
- 			return res.negotiate(err);
- 		});
+				smasher.placings[i] = await Placing.findOne({ id: smasher.placings[i].id }).populate('tournament').populate('smasher');
+			}
+
+			return res.view('smasher/index', { smasher: smasher, community: smasher.community });
+		} catch (err) {
+			return res.negotiate(err);
+		}
  	},
 
  	edit: function(req, res) {
